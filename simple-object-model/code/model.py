aa17788e3cff0223c0be8bee6c621d32ada8a520
jinguanz <berkzhou@gmail.com>
2016-06-23 11:06:38 -0700

    Add model object chapter

---
+++ simple-object-model/code/model.py
@@ -0,0 +1,203 @@
+def test_read_write_field():
+    # python code
+    class A(object):
+        pass
+    obj = A()
+    obj.a = 1
+    assert obj.a == 1
+
+    obj.b = 5
+    assert obj.a == 1
+    assert obj.b == 5
+
+    obj.a = 2
+    assert obj.a == 2
+    assert obj.b == 5
+
+    # object model code
+    A = Class(name="A", base_class=OBJECT, fields={}, metaclass=TYPE)
+    obj = Instance(A)
+    obj.write_attr("a", 1)
+    assert obj.read_attr("a") == 1
+
+    obj.write_attr("b", 5)
+    assert obj.read_attr("a") == 1
+    assert obj.read_attr("b") == 5
+
+    obj.write_attr("a", 2)
+    assert obj.read_attr("a") == 2
+    assert obj.read_attr("b") == 5
+
+def test_read_write_field_class():
+    # classes are objects too.
+    # Test reading and writing attributes works on classes as well
+
+    class A(object):
+        pass
+
+    A.a = 1
+    assert A.a == 1
+    A.a = 6
+    assert A.a == 6
+
+    A = Class(name="A", base_class=OBJECT, fields={"a": 1}, metaclass=TYPE)
+    assert A.read_attr("a") == 1
+    A.write_attr("a", 5)
+    assert A.read_attr("a") == 5
+
+def test_isinstance():
+    # python code
+    class A(object):
+        pass
+    class B(A):
+        pass
+    b = B()
+    assert isinstance(b, B)
+    assert isinstance(b, A)
+    assert isinstance(b, object)
+    assert not isinstance(b, type)
+
+    # Object model code
+    A = Class(name="A", base_class=OBJECT, fields={}, metaclass=TYPE)
+    B = Class(name="B", base_class=A, fields={}, metaclass=TYPE)
+    b = Instance(B)
+    assert b.isinstance(B)
+    assert b.isinstance(A)
+    assert b.isinstance(OBJECT)
+    assert not b.isinstance(TYPE)
+
+def test_callmethod_simple():
+    class A(object):
+        def f(self):
+            return self.x + 1
+
+    obj = A()
+    obj.x = 1
+    assert obj.f() == 2
+
+    class B(A):
+        pass
+    obj = B()
+    obj.x = 1
+    assert obj.f() == 2
+
+    def f_A(self):
+        return self.read_attr("x") + 1
+    A = Class(name="A", base_class=OBJECT, fields={"f": f_A}, metaclass=TYPE)
+    obj = Instance(A)
+    obj.write_attr("x", 1)
+    assert obj.callmethod("f") == 2
+
+    B = Class(name="B", base_class=A, fields={}, metaclass=TYPE)
+    obj = Instance(B)
+    obj.write_attr("x", 2)
+    assert obj.callmethod("f") == 3
+
+
+def test_callmethod_subclassing_and_arguments():
+    # Python code
+    class A(object):
+        def g(self, arg):
+            return self.x  +arg
+
+    obj = A()
+    obj.x = 1
+    assert obj.g(4) == 5
+
+    class B(A):
+        def g(self, arg):
+            return self.x  +arg * 2
+    obj = B()
+    obj.x = 4
+    assert obj.g(4) == 12
+
+    # Object model code
+    def g_A(self, arg):
+        return self.read_attr("x") + arg
+    A = Class(name="A", base_class=OBJECT, fields={"g": g_A}, metaclass=TYPE)
+    obj = Instance(A)
+    obj.write_attr("x", 1)
+    assert obj.callmethod("g", 4) == 5
+
+    def g_B(self, arg):
+        return self.read_attr("x") + arg * 2
+    B = Class(name="B", base_class=A, fields={"g": g_B}, metaclass=TYPE)
+    obj = Instance(B)
+    obj.write_attr("x", 4)
+    assert obj.callmethod("g", 4) == 12
+
+
+class Base(object):
+    """ The base class that all of the object model classes inherit from. """
+
+    def __init__(self, cls, fields):
+        self.cls = cls
+        self._fields = fields
+
+    def read_attr(self, fieldname):
+        return self._read_dict(fieldname)
+
+    def write_attr(self, fieldname, value):
+        self._write_dict(fieldname, value)
+
+    def isinstance(self, cls):
+        return self.cls.issubclass(cls)
+
+    def callmethod(self, methname, *args):
+        meth = self.cls._read_from_class(methname)
+        return meth(self, *args)
+
+    def _read_dict(self, fieldname):
+        return self._fields.get(fieldname, MISSING)
+
+    def _write_dict(self, fieldname, value):
+        self._fields[fieldname] = value
+
+MISSING = object()
+
+class Instance(Base):
+    """ Instance of a user-defined class. """
+
+    def __init__(self, cls):
+        assert isinstance(cls, Class)
+        Base.__init__(self, cls, {})
+
+class Class(Base):
+    """ A User-defined class. """
+
+    def __init__(self, name, base_class, fields, metaclass):
+        Base.__init__(self, metaclass, fields)
+        self.name = name
+        self.base_class = base_class
+
+    def method_resolution_order(self):
+        if self.base_class is None:
+            return [self]
+        else:
+            return [self] + self.base_class.method_resolution_order()
+
+    def issubclass(self, cls):
+        return cls in self.method_resolution_order()
+
+    def _read_from_class(self, methname):
+        for cls in self.method_resolution_order():
+            if methname in cls._fields:
+                return cls._fields[methname]
+        return MISSING
+
+# set up the base hierarchy as in Python (the ObjVLisp model)
+# the ultimate base class is OBJECT
+OBJECT = Class(name="object", base_class=None, fields={}, metaclass=None)
+# TYPE is a subclass of OBJECT
+TYPE = Class(name="type", base_class=OBJECT, fields={}, metaclass=None)
+# TYPE is an instance of itself
+TYPE.cls = TYPE
+# OBJECT is an instance of TYPE
+OBJECT.cls = TYPE
+
+
+test_read_write_field()
+test_read_write_field_class()
+test_isinstance()
+test_callmethod_simple()
+test_callmethod_subclassing_and_arguments()
